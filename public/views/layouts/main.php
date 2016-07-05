133563cb755e8a46d952f3bd3c5378c38823f590
Matt Stephens <matthewstephens@digidev.co.uk>
2016-07-05 11:30:45 +0100

    Altered main layout to work with json
    Should probably have a json layout

--- public/views/layouts/main.php
+++ public/views/layouts/main.php
@@ -1,13 +1,15 @@
 <?php
-// Starting a sessions for persisting of data
+// Starting a sessions for persisting of data and displaying flash messages
 session_start();
 
 // Loading in Route Helper
 use App\Route;
-require_once('./helpers/RouterController.php');
+require_once('./helpers/Router.php');
 $route = new App\Helper\Route\Router;
 // Making a route request
 $request = $route->request($_SERVER['REQUEST_URI']);
+
+if (!stristr($request['page'], 'json')) {
 
 ?>
 
@@ -16,7 +18,7 @@ $request = $route->request($_SERVER['REQUEST_URI']);
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width">
-	<title>My Directory | <?php echo $request['title'] ?></title>
+	<title>My Directory<?php echo ' | '.$request['title'] ?></title>
 	<link rel="stylesheet" href="/resources/css/semantic.min.css" />
 	<link rel="stylesheet" href="/resources/css/styles.css" />
 </head>
@@ -30,10 +32,19 @@ $request = $route->request($_SERVER['REQUEST_URI']);
 			include('shared/messages.php');
 			// Load in respose from Route Helper
 			require_once($request['page']);
+			// Footer and Nav
+			include('shared/footer.php');
 		 ?>
 
 	</div>
 	<script type="text/javascript" src="/resources/js/jquery.min.js"></script>
+	<script type="text/javascript" src="/resources/js/semantic.min.js"></script>
 	<script type="text/javascript" src="/resources/js/script.js"></script>
 </body>
 </html>
+
+<?php
+} else {
+	include($request['page']);
+}
+?>
