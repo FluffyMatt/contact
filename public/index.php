5c7f8363229efec32325b0c79901f950d585ff3f
Matt Stephens <matthewstephens@digidev.co.uk>
2016-07-05 11:32:16 +0100

    Fix a dumb mistake echo out a require :(

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 	ini_set("display_errors", "on");
 	error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
 	require_once('controllers/ContactDirectory.php');
+	// Load in namespace for class use
 	use App\Contact;
 
-	echo require_once(getcwd()."/views/layouts/main.php");
+	// Load the main layout
+	require_once(getcwd()."/views/layouts/main.php");
