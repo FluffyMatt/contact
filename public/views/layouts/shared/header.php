1ebe22618a61667efc58f59860457a4e1942acc8
Matt Stephens <matthewstephens@digidev.co.uk>
2016-07-05 11:30:17 +0100

    Altered search in header

--- public/views/layouts/shared/header.php
+++ public/views/layouts/shared/header.php
@@ -5,9 +5,11 @@
 		<a class="item" href="/contacts/add">Add contact</a>
 		<div class="item right">
 			<form id="search" class="ui form" action="/contacts/search" method="get">
-				<div class="ui icon input">
-					<i class="search icon link"></i>
-					<input type="text" name="lookup" id="lookup" class="lookup" placeholder="Search for a contact" />
+				<div class="ui search">
+					<div class="ui icon input">
+						<input type="text" name="lookup" id="lookup" class="prompt lookup" placeholder="Search for a contact" />
+						<i class="search icon link"></i>
+					</div>
 				</div>
 			</form>
 		</div>
