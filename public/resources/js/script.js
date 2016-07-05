c19980d8489d2fd8953d2b03ce7ff509308e9ace
Matt Stephens <matthewstephens@digidev.co.uk>
2016-07-05 11:31:41 +0100

    Async search added

--- public/resources/js/script.js
+++ public/resources/js/script.js
@@ -5,10 +5,27 @@ $(document).ready(function() {
 		$('#search').submit();
 	})
 
+	// Search json using semantic UI
+	$('.ui.search').search({
+		apiSettings: {
+			method: 'POST',
+	    	url: '/contacts/json',
+			searchFields   : [
+		    	'title'
+		    ],
+			beforeSend: function (settings) {
+				settings.data.name = $('input[name="lookup"]').val();
+		        return true;
+		    },
+	    }
+	});
+
+	// Add favourite
 	$('.star.icon:not(.active)').click(function() {
 		$(this).next('.contact-fav').submit();
 	})
 
+	// Close messages
 	$('.close.icon').click(function() {
 		$(this).parent().fadeOut('fast');
 		$(this).parent().remove;
